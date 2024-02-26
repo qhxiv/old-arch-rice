e5a6aef4ebba918656c321045a6d6fa9e91f73c8
qhxiv <eknight1412@gmail.com>
2024-02-26 10:25:07 +0700

    remove opactity for inactive windows, add alpha patch for dmenu

--- .config/suckless/dmenu/config.h
+++ .config/suckless/dmenu/config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xc0;     /* Amount of opacity. oxff is opaque */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 250;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -14,6 +15,12 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#f5efd0", "#191724" },
 	[SchemeSel]  = { "#f5efd0", "#26233a" },
 	[SchemeOut]  = { "#f5efd0", "#26233a" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel]  = { OPAQUE, alpha },
+	[SchemeOut]  = { OPAQUE, alpha },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
